c209c83a17fad0f744e9ed0c00f00e01c2d6adb1
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-13 11:16:50 +0700

    dung @JsonProperty de config lai field trong response data

--- demo/src/main/java/com/example/demo/Domain/DTO/UserDataResponseLoginSuccessfullyDTO.java
+++ demo/src/main/java/com/example/demo/Domain/DTO/UserDataResponseLoginSuccessfullyDTO.java
@@ -1,10 +1,15 @@
 package com.example.demo.Domain.DTO;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserDataResponseLoginSuccessfullyDTO {
+
+    @JsonProperty("access_token")
     private String accessToken;
+
+    @JsonProperty("user")
     private UserDataLoginSuccessfullyDTO userDataLoginSuccessfullyDTO;
 
     public UserDataLoginSuccessfullyDTO getUserDataLoginSuccessfullyDTO() {
